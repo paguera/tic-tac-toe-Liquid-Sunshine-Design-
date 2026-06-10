bce04f8fbdf8f6df1473c06da6c831ba5b4151c0
Gabriel VF <gab.vf.93@gmail.com>
2026-06-10 20:01:46 +0200

    added picture

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ main();
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Jeu Morpion - LSD Edition</title>
+  <title>Morpion</title>
   <style>
     <?php include('style.css'); ?>
   </style>
